10e86ad27099f13090b7b6ca6bc1741ad6d57505
Kari Rice <kari.rice@gmail.com>
2023-03-09 23:05:12 -0700

    make chains function

--- markov.py
+++ markov.py
@@ -11,7 +11,7 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    text_file = open(file_path, mode='r')
+    text_file = open(file_path, mode='r') #mode=r means you can read but not edit file when opened
     all_of_it = text_file.read()
 
    
@@ -47,39 +47,61 @@ def make_chains(text_string) :
 
    # text_string is all_of_it
    # split string by whitespace words = split string
-    words = text_string.split(" ")
+    
+    words = text_string.split() #leave blank to capture all white space characters
+
 
     for i in range(len(words) - 2):
-        chains_key = (words[i], words[i+1])
-        chains_value = words[i + 2]
-
-        chains[chains_key] = chains_value
-
-
-    return chains
-
-# make_chains(open_and_read_file(file_path))
-
-
-# def make_text(chains):
-#     """Return text from chains."""
-
-#     words = []
-
-#     # your code goes here
-
-#     return ' '.join(words)
+        
+        chains_key = (words[i], words[i+1]) #{('could', 'you')}
+        # print(chains_key)
+        chains_value = words[i + 2] #"you"
+       
+        if chains_key not in chains:
+            chains[chains_key] = []
+            
+        chains[chains_key].append(chains_value)
 
 
-# input_path = 'green-eggs.txt'
+    print(chains)
 
-# # Open the file and turn it into one long string
-# input_text = open_and_read_file(input_path)
+make_chains(open_and_read_file(file_path))
 
-# # Get a Markov chain
-# chains = make_chains(input_text)
 
-# # Produce random text
-# random_text = make_text(chains)
+""" 
 
-# print(random_text)
+dict = {'blue', [1, 3, 5]}
+
+dict[blue] = 
+
+
+
+"""
+
+
+
+
+
+
+def make_text(chains):
+    """Return text from chains."""
+
+    words = []
+
+    
+
+    return ' '.join(words)
+
+
+input_path = 'green-eggs.txt'
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+print(random_text)
